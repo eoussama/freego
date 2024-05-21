e4adc4e80a51c34f8b46409a48a22b500ef038b7
eoussama <oussama.essamadi@gmail.com>
2024-05-21 16:56:20 +0100

    fix: modification in place of copying, #23

--- src/models/config.go
+++ src/models/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	ApiKey    string
 }
 
-func (c Config) Build(options *Options) Config {
+func (c Config) Build(options *Options) *Config {
 	var resolveOption = func(optionValue string, fallbackValue string) string {
 		if options != nil && len(optionValue) > 0 {
 			return optionValue
@@ -20,14 +20,14 @@ func (c Config) Build(options *Options) Config {
 		}
 	}
 
-	return Config{
-		IsPartner: options.FreestuffPartner,
-		Url:       "https://api.freestuffbot.xyz/v1",
-		Port:      resolveOption(options.WebhookPort, c.getEnv("FREEGO_WEBHOOK_PORT", "8080")),
-		Secret:    resolveOption(options.WebhookSecret, c.getEnv("FREEGO_WEBHOOK_SECRET", "")),
-		ApiKey:    resolveOption(options.FreestuffApiKey, c.getEnv("FREEGO_FREESTUFF_API_KEY", "")),
-		Route:     resolveOption(options.WebhookRoute, c.getEnv("FREEGO_WEBHOOK_ROUTE", "/webhook")),
-	}
+	c.IsPartner = options.FreestuffPartner
+	c.Url = "https://api.freestuffbot.xyz/v1"
+	c.Port = resolveOption(options.WebhookPort, c.getEnv("FREEGO_WEBHOOK_PORT", "8080"))
+	c.Secret = resolveOption(options.WebhookSecret, c.getEnv("FREEGO_WEBHOOK_SECRET", ""))
+	c.ApiKey = resolveOption(options.FreestuffApiKey, c.getEnv("FREEGO_FREESTUFF_API_KEY", ""))
+	c.Route = resolveOption(options.WebhookRoute, c.getEnv("FREEGO_WEBHOOK_ROUTE", "/webhook"))
+
+	return &c
 }
 
 func (c Config) getEnv(key string, fallback string) string {
